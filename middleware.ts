0fc0de53e3be182363a086f104cc17c49d5f5b8d
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2026-02-21 21:12:21 +0900

    feat: 不要なマッチャーをコメントアウトして整理

--- middleware.ts
+++ middleware.ts
@@ -44,6 +44,7 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    // "/((?!_next/static|_next/image|favicon.ico).*)",
+    ["/((?!_next|favicon.ico).*)"],
   ],
 };
