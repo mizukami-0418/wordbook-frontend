d5fa53b342a945cabcc9d68ecb656bfb78fea299
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2026-02-21 21:38:03 +0900

    feat: matcherを簡素化して全てのパスを許可する

--- middleware.ts
+++ middleware.ts
@@ -42,9 +42,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    // "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-    // "/((?!_next/static|_next/image|favicon.ico).*)",
-    ["/:path*"],
-  ],
+  matcher: ["/:path*"],
 };
